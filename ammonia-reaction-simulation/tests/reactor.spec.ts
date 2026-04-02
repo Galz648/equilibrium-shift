374bc08744708bdd59a74195ebc4110f500c8d3f
Gal Zafar <theycallmefachfach@gmail.com>
2026-04-03 00:51:28 +0300

    fix:  tests pass feature: simulation diagnostics UI

--- ammonia-reaction-simulation/tests/reactor.spec.ts
+++ ammonia-reaction-simulation/tests/reactor.spec.ts
@@ -1,6 +1,8 @@
 import type { ReactorState } from "../src/reactor"
-import { updateReactorState } from "../src/simulate"
+import { updateReactorState, type Conditions, type Controls, createInitialConditions } from "../src/simulate"
 import type { SimulatorState } from "../src/simulator"
+
+
 describe("Reactor", () => {
     // TODO: create a test for the edge cases
 
@@ -8,178 +10,93 @@ describe("Reactor", () => {
         describe("Trivial Steady States", () => {
 
             test("temperature remains unchanged at equilibrium without heat input", () => {
-                const initial_conditions: ReactorState = {
-                    N2: 0,
-                    H2: 0,
-                    NH3: 0,
+                const conditions = createInitialConditions({
                     T: 298
-                }
+                } as Partial<Conditions>)
 
-                const sim_state: SimulatorState = {
-                    t: 0,
-                    dt: 0.001,
-                    dH2: 0,
-                    dNH3: 0,
-                    dN2: 0,
-                    dT: 0,
-                }
+                const state = updateReactorState(conditions)
 
-                const state = updateReactorState({
-                    reactor_state: initial_conditions,
-                    simulator_state: sim_state,
-                    controls: { heat_input: 0 }
-                })
-
-                expect(state.T).toBe(initial_conditions.T)
+                expect(state.T).toBe(conditions.reactor_state.T)
             })
-        })
 
-        describe("Non-Trivial Steady States", () => {
-            test("temperature remains unchanged at reaction equilibrium without heat input", () => {
-                const initial_conditions: ReactorState = { // TODO: determine if this is actually a steady state
-                    N2: 1,
-                    H2: 3,
-                    NH3: 2,
-                    T: 298
-                }
-
-                const sim_state: SimulatorState = {
-                    t: 0,
-                    dt: 0.001,
-                    dH2: 0,
-                    dNH3: 0,
-                    dN2: 0,
-                    dT: 0,
-                }
-
-                const state = updateReactorState({
-                    reactor_state: initial_conditions,
-                    simulator_state: sim_state,
-                    controls: { heat_input: 0 }
-                })
-
-                expect(state.T).toBe(initial_conditions.T)
-                expect(state.N2).toBe(initial_conditions.N2)
-                expect(state.H2).toBe(initial_conditions.H2)
-
-            })
-        })
-        // // 
-
-        describe("Invariants", () => {
             test("concentrations remain unchanged with no reactants or products", () => {
-                const initial_conditions: ReactorState = {
-                    N2: 0,
-                    H2: 0,
-                    NH3: 0,
-                    T: 298
-                }
+                const conditions = createInitialConditions({
+                } as Partial<Conditions>)
 
-                const sim_state: SimulatorState = {
-                    t: 0,
-                    dt: 0.001,
-                    dH2: 0,
-                    dNH3: 0,
-                    dN2: 0,
-                    dT: 0,
-                }
 
-                const state = updateReactorState({
-                    reactor_state: initial_conditions,
-                    simulator_state: sim_state,
-                    controls: { heat_input: 0 }
-                })
+                const state = updateReactorState(conditions)
 
-                expect(state.N2).toBe(initial_conditions.N2)
-                expect(state.H2).toBe(initial_conditions.H2)
-                expect(state.NH3).toBe(initial_conditions.NH3)
-                expect(state.T).toBe(initial_conditions.T)
+                expect(state.N2).toBe(conditions.reactor_state.N2)
+                expect(state.H2).toBe(conditions.reactor_state.H2)
+                expect(state.NH3).toBe(conditions.reactor_state.NH3)
+                expect(state.T).toBe(conditions.reactor_state.T)
             })
 
 
             test("temperature remains unchanged at equilibrium without heat input", () => {
-                const initial_conditions: ReactorState = {
-                    N2: 0,
-                    H2: 0,
-                    NH3: 0,
-                    T: 298
-                }
+                const conditions = createInitialConditions({
+                } as Partial<Conditions>)
 
-                const sim_state: SimulatorState = {
-                    t: 0,
-                    dt: 0.001,
-                    dH2: 0,
-                    dNH3: 0,
-                    dN2: 0,
-                    dT: 0,
-                }
+                const state = updateReactorState(conditions)
 
-                const state = updateReactorState({
-                    reactor_state: initial_conditions,
-                    simulator_state: sim_state,
-                    controls: { heat_input: 0 }
-                })
-
-                expect(state.T).toBe(initial_conditions.T)
+                expect(state.T).toBe(conditions.reactor_state.T)
             })
+
         })
 
+
+        describe("Non-Trivial Steady States", () => {
+            test("temperature remains unchanged at reaction equilibrium without heat input", () => {
+                const conditions = createInitialConditions({
+                    reactor_state: {
+                        N2: 1,
+                        H2: 3,
+                        NH3: 2,
+                    }
+                } as Partial<Conditions>)
+
+                const state = updateReactorState(conditions)
+
+                expect(state.T).toBe(conditions.reactor_state.T)
+                expect(state.N2).toBe(conditions.reactor_state.N2)
+                expect(state.H2).toBe(conditions.reactor_state.H2)
+
+            })
+        })
 
         describe("Reaction Dynamics", () => {
             describe("Forward Reaction", () => {
                 test("increases product concentration in forward reaction", () => { // TODO: problematic assumption that the forward rate occurs
-                    const initial_conditions: ReactorState = {
-                        N2: 1,
-                        H2: 3,
-                        NH3: 0,
-                        T: 700
-                    }
+                    const conditions = createInitialConditions({
+                        reactor_state: {
+                            N2: 1,
+                            H2: 3,
+                            NH3: 0,
+                            T: 700
+                        }
 
-                    const sim_state: SimulatorState = {
-                        t: 0,
-                        dt: 0.001,
-                        dH2: 0,
-                        dNH3: 0,
-                        dN2: 0,
-                        dT: 0,
-                    }
+                    } as Partial<Conditions>)
 
-                    const state = updateReactorState({
-                        reactor_state: initial_conditions,
-                        simulator_state: sim_state,
-                        controls: { heat_input: 0 }
-                    })
+                    const state = updateReactorState(conditions)
 
-                    expect(state.NH3).toBeGreaterThan(initial_conditions.NH3)
-                    expect(state.H2).toBeLessThan(initial_conditions.H2)
-                    expect(state.N2).toBeLessThan(initial_conditions.N2)
+                    expect(state.NH3).toBeGreaterThan(conditions.reactor_state.NH3)
+                    expect(state.H2).toBeLessThan(conditions.reactor_state.H2)
+                    expect(state.N2).toBeLessThan(conditions.reactor_state.N2)
                 })
             })
             describe("Backward Reaction", () => {
                 test("decreases product concentration in backward reaction", () => {
-                    const initial_conditions: ReactorState = {
-                        N2: 0,
-                        H2: 0,
-                        NH3: 1,
-                        T: 700
-                    }
-                    const sim_state: SimulatorState = {
-                        t: 0,
-                        dt: 0.001,
-                        dH2: 0,
-                        dNH3: 0,
-                        dN2: 0,
-                        dT: 0,
-                    }
-                    const state = updateReactorState({
-                        reactor_state: initial_conditions,
-                        simulator_state: sim_state,
-                        controls: { heat_input: 0 }
-                    })
+                    const conditions = createInitialConditions({
+                        reactor_state: {
+                            NH3: 1,
+                            T: 700
+                        }
+                    } as Partial<Conditions>)
+                    const state = updateReactorState(conditions)
 
-                    expect(state.N2).toBeGreaterThan(initial_conditions.N2)
-                    expect(state.H2).toBeGreaterThan(initial_conditions.H2)
-                    expect(state.NH3).toBeLessThan(initial_conditions.NH3)
+                    expect(state.N2).toBeGreaterThan(conditions.reactor_state.N2)
+                    expect(state.H2).toBeGreaterThan(conditions.reactor_state.H2)
+                    expect(state.NH3).toBeLessThan(conditions.reactor_state.NH3)
                 })
                 // placeholder for symmetry
                 // test("increases reactant concentration in reverse reaction", ...)
@@ -190,55 +107,31 @@ describe("Reactor", () => {
         describe("Control Effects", () => {
             describe("Heat Input", () => {
                 test("temperature increases with heat input", () => {
-                    const initial_conditions: ReactorState = {
-                        N2: 0,
-                        H2: 0,
-                        NH3: 0,
-                        T: 700
-                    }
+                    const conditions = createInitialConditions({
+                        reactor_state: {
+                            T: 700
+                        },
+                        controls: {
+                            heat_input: 100 // TODO: create a heater object, to define the heat input (max, min, default)
+                        }
+                    } as Partial<Conditions>)
 
-                    const sim_state: SimulatorState = {
-                        t: 0,
-                        dt: 0.001,
-                        dH2: 0,
-                        dNH3: 0,
-                        dN2: 0,
-                        dT: 0,
-                    }
 
-                    const state = updateReactorState({
-                        reactor_state: initial_conditions,
-                        simulator_state: sim_state,
-                        controls: { heat_input: 100 }
-                    })
+                    const state = updateReactorState(conditions)
 
-                    expect(state.T).toBeGreaterThan(initial_conditions.T)
+                    expect(state.T).toBeGreaterThan(conditions.reactor_state.T)
                 })
             })
             test("temperature decreases without heat input, when temperature is above equilibrium, and no reaction occurs", () => {
-                const initial_conditions: ReactorState = {
-                    N2: 0,
-                    H2: 0,
-                    NH3: 0,
-                    T: 300
-                }
+                const conditions = createInitialConditions({
+                    reactor_state: {
+                        T: 400
+                    }
+                } as Partial<Conditions>)
 
-                const sim_state: SimulatorState = {
-                    t: 0,
-                    dt: 0.001,
-                    dH2: 0,
-                    dNH3: 0,
-                    dN2: 0,
-                    dT: 0,
-                }
+                const state = updateReactorState(conditions)
 
-                const state = updateReactorState({
-                    reactor_state: initial_conditions,
-                    simulator_state: sim_state,
-                    controls: { heat_input: 0 }
-                })
-
-                expect(state.T).toBeLessThan(initial_conditions.T)
+                expect(state.T).toBeLessThan(conditions.reactor_state.T)
             })
         })
 
